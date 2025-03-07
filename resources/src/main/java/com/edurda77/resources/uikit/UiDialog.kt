23547ed839e5240156fc9c4cc527cabd791fc3a0
Impuls <edurd@rambler.ru>
2025-03-07 11:22:08 +0500

    UiBottomNavigation

--- resources/src/main/java/com/edurda77/resources/uikit/UiDialog.kt
+++ resources/src/main/java/com/edurda77/resources/uikit/UiDialog.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -21,9 +20,9 @@ fun UiDialog(
     Dialog(onDismissRequest = onCloseDialog) {
         Column(
             modifier = modifier
-                .clip(shape = RoundedCornerShape(10.dp))
+                .clip(shape = MaterialTheme.shapes.medium)
                 .fillMaxWidth()
-                .background(color = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.7f))
+                .background(color = MaterialTheme.colorScheme.background)
                 .padding(10.dp)
         ) {
             content()
